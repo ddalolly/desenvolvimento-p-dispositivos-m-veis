4156e00a3798146a66fa2e35a898283b17c894a0
ddalolly <113464582+ddalolly@users.noreply.github.com>
2023-04-12 13:23:10 -0300

    Add files via upload

---
+++ testefor.js
@@ -0,0 +1,7 @@
+/* teste utilizando o laço de repetição for */
+
+let i=5000 //é um escopo global, aparece no código inteiro
+for (let i= 1000; i< 5000; i+=100) {
+    console.log(i);
+}
+console.log(`UFA, até que enfim estou ganhando ${i} reais!!!`);
